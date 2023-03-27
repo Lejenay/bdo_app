97814a3af1c0d338a9434227d20b9bcd9e7b4fe8
Lejenay <lejenay23@gmail.com>
2023-03-27 22:55:48 +0900

    minor update

--- app.py
+++ app.py
@@ -17,16 +17,12 @@ def acc_api_call(item_id):
     }
 
     response = requests.request('POST', url, json=payload, headers=headers)
-
     res_letter = response.text
     res_list = re.split(r'[\-|:]', res_letter)
-    del res_list[0]
-    del res_list[0]
+    del res_list[0:2]
     del res_list[-1]
 
-    price_list = []
-    for n in range(6):
-        price_list.append(int(res_list[10*n+3]))
+    price_list = [int(res_list[10 * n + 3]) for n in range(6)]
     return price_list
 
 
@@ -45,8 +41,7 @@ def item_api_call(item_id):
     response = requests.request('POST', url, json=payload, headers=headers)
     res_letter = response.text
     res_list = re.split(r'[\-|:]', res_letter)
-    del res_list[0]
-    del res_list[0]
+    del res_list[0:2]
     del res_list[-1]
     return int(res_list[3])
 
